5d36789773ed169236afd4be39eb8c4aa5e5edb6
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-08-23 16:58:14 -0300

    feat: create file upload component

---
+++ src/components/Form/FileInput/Control.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { ComponentProps } from "react";
+import { useFileInput } from "./Root";
+
+export type ControlProps = ComponentProps<"input">;
+
+export function Control(props: ControlProps) {
+  const { id } = useFileInput();
+
+  return (
+    <input
+      type='file'
+      className='sr-only'
+      id={id}
+      {...props}
+    />
+  );
+}
---
+++ src/components/Form/FileInput/Root.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { ComponentProps, createContext, useContext, useId } from "react";
+
+export type RootProps = ComponentProps<"div">;
+
+type FileInputContextType = {
+  id: string;
+};
+
+const FileInputContext = createContext({} as FileInputContextType);
+
+export function Root(props: RootProps) {
+  const id = useId();
+
+  return (
+    <FileInputContext.Provider value={{ id }}>
+      <div {...props} />
+    </FileInputContext.Provider>
+  );
+}
+
+export const useFileInput = () => useContext(FileInputContext);
